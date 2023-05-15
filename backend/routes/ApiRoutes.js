2b3f3179178a940ac9c14b74cb432f2026e33937
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-05-15 08:42:11 -0300

    clean

--- backend/routes/ApiRoutes.js
+++ backend/routes/ApiRoutes.js
@@ -1,31 +1,16 @@
 const router = require('express').Router()
 
-const SSE = require('sse')
-
-
 const ProductController = require('../controllers/ProductCotroller')
 const PackController = require('../controllers/PackController')
 const CsvController = require('../controllers/CsvController')
-const TesteController = require('../controllers/TesteController')
 
 const { fileUpload } = require('../helpers/file-upload')
 
 // start route
 router.post('/home', fileUpload.single('csv_file'), CsvController.sendFile) //
 
-// router.get('/analise', ProductController.getAllProducts)
 router.get('/analise', ProductController.newPrices)
 
-router.get('/events', function (req, res) {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // cria um objeto SSE
-  const sse = new SSE(res);
-
-  // envia a mensagem para o cliente
-  sse.send(`O novo preço do produto ${productToUpdate.name} é menor do que o seu custo.`);
-});
 
 // product routes
 router.get('/products', ProductController.getAllProducts)
@@ -33,8 +18,5 @@ router.get('/products', ProductController.getAllProducts)
 // pack routes
 router.get('/packs', PackController.getAllPacks)
 
-
-//  rota de teste
-router.get('/teste', TesteController.teste)
 
 module.exports = router
